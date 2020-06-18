6ba21326a8d50bc07d4a68022cb7e531d47d549e
Alex <motzart66@gmail.com>
2020-06-18 12:16:40 +0300

    Add icon to main menu

---
+++ src/config/constants.js
@@ -0,0 +1,8 @@
+// page name
+export const OVERVIEW = 'overview';
+export const EPOCHS = 'epochs';
+export const LAYERS = 'layers';
+export const TXNS = 'txns';
+export const REWARDS = 'rewards';
+export const ACCOUNTS = 'accounts';
+export const SMESHER = 'smesher';
