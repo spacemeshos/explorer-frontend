2ed7541ea87316e091de1cfd6bd4c84c9157f676
Alex <motzart66@gmail.com>
2020-07-29 18:18:25 +0300

    Change loader

--- src/components/common/Loader/index.js
+++ src/components/common/Loader/index.js
@@ -1,6 +1,6 @@
 // @flow
 import * as React from 'react';
-import loader from "../../../assets/loader.gif";
+import loader from '../../../assets/loader.gif';
 
 type Props = {
   size: number;
@@ -8,13 +8,13 @@ type Props = {
 
 const Loader = (props: Props) => {
   const { size } = props;
-  const loaderSize = size ? size : 15;
+  const loaderSize = size || 15;
 
   return (
-    <span style={{textAlign: 'center'}}>
-      <img style={{width: `${loaderSize}px`}} src={loader} alt="loading..." />
+    <span style={{ textAlign: 'center' }}>
+      <img style={{ width: `${loaderSize}px` }} src={loader} alt="loading..." />
     </span>
-  )
+  );
 };
 
 export default Loader;
