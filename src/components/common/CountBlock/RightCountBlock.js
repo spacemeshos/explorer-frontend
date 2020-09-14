c03abafca46b58d66a4fe24d0e0a26d4f59b7159
Alex <motzart66@gmail.com>
2020-09-14 08:58:27 +0300

    Rework rewards right module

--- src/components/common/CountBlock/RightCountBlock.js
+++ src/components/common/CountBlock/RightCountBlock.js
@@ -1,6 +1,6 @@
 // @flow
 import * as React from 'react';
-import {smhCoinConverter} from '../../../helper/converter';
+import { smhCoinConverter } from '../../../helper/converter';
 
 type Props = {
   color: string,
@@ -12,9 +12,28 @@ type Props = {
 
 const RightCountBlock = (props: Props) => {
   const { color, number,  caption, coinCaption, coins} = props;
+
+  const setFontSize = (number) => {
+    if (number && number.length < 5) return '60px';
+    if (number && number.length >= 5) return '42px';
+    if (number && number.length > 8) return '34px';
+  };
+
+  const setLineHeight = (number) => {
+    if (number && number.length < 5) return '42px';
+    if (number && number.length >= 5) return '36px';
+    if (number && number.length > 8) return '28px';
+  };
+
   return (
     <div className="rightColumn" style={{ backgroundColor: color.bgColor }}>
-      <div className="rightColumn-number" style={{ color: color.textColor }}>{number  || '000'}</div>
+      <div
+        className="rightColumn-number"
+        style={{
+          color: color.textColor,
+          fontSize: setFontSize(number),
+          lineHeight: setLineHeight(number)
+        }}>{number  || '000'}</div>
       <div className="rightColumn-desc">{caption}</div>
       <div className="rightColumn-data">
         <p> {`${coinCaption}: `}</p>
