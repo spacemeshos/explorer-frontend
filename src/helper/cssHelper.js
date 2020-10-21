4b8e37fd471eb70692fcb44d00b8893efcc59aa2
despablos <pawel777yac@gmail.com>
2020-10-21 16:51:09 +0300

    Fixed space between text & font-size

--- src/helper/cssHelper.js
+++ src/helper/cssHelper.js
@@ -1,14 +1,14 @@
 export const setFontSize = (number) => {
   const num = JSON.stringify(number);
   if (num && num.length < 4) return '60px';
-  if (num && num.length >= 4) return '42px';
+  if (num && num.length >= 4) return '38px';
   if (num && num.length > 8) return '34px';
 };
 
 export const setLineHeight = (number) => {
   const num = JSON.stringify(number);
   if (num && num.length < 4) return '42px';
-  if (num && num.length >= 4) return '36px';
+  if (num && num.length >= 4) return '30px';
   if (num && num.length > 8) return '28px';
 };
 
