88a020ece8752734ddccead86b87d9626daa8fcf
Kacper Sawicki <kacper@ksawicki.pl>
2023-11-23 15:51:08 +0100

    Refactor blocks list page

--- src/components/Table/BlocksRow.jsx
+++ src/components/Table/BlocksRow.jsx
@@ -1,22 +1,12 @@
 // @flow
 import { nanoid } from 'nanoid';
-import { Link } from 'react-router-dom';
-import longFormHash from '../../helper/longFormHash';
-import { BLOCKS } from '../../config/constants';
-import CustomTimeAgo from '../CustomTimeAgo';
 import { formatSmidge } from '../../helper/converter';
 
 const BlocksRow = ({ data }) => (
   data.map((item) => (
     <div key={nanoid()} className="tr">
       <div className="td">
-        <Link to={`/${BLOCKS}/${item.id}`}>
-          {longFormHash(item.id)}
-        </Link>
-      </div>
-      <div className="td">--</div>
-      <div className="td">
-        <CustomTimeAgo time={item.start} />
+        {item.id}
       </div>
       <div className="td">{item.txsnumber}</div>
       <div className="td">{formatSmidge(item.txsvalue)}</div>
