c267ba4c47a7d32d873678a3f8c64e543629b48e
Kacper Sawicki <kacper@ksawicki.pl>
2024-03-22 14:44:40 +0100

    Change color of future epoch row

--- src/components/Table/EpochsRow.jsx
+++ src/components/Table/EpochsRow.jsx
@@ -12,28 +12,35 @@ import CustomTimeAgo from '../CustomTimeAgo';
 import { formatSmidge } from '../../helper/converter';
 
 const EpochsRow = ({ data }) => (
-  data && data.map((item) => (
-    <div key={nanoid()} className="tr">
-      <div className="td"><Link to={`/${EPOCHS}/${item.number}`}>{item.number}</Link></div>
-      <div className="td">
-        <CustomTimeAgo time={item.start} />
+  data && data.map((item) => {
+    const futureEpoch = item.start * 1000 > Date.now();
+    return (
+      <div key={nanoid()} className={`tr ${futureEpoch ? 'futureEpoch' : ''}`}>
+        <div className="td">
+          <Link to={`/${EPOCHS}/${item.number}`}>
+            {item.number}
+          </Link>
+        </div>
+        <div className="td">
+          <CustomTimeAgo time={item.start} />
+        </div>
+        <div className="td">
+          <Link to={`/${EPOCHS}/${item.number}/${LAYERS}`}>{item.layers}</Link>
+        </div>
+        <div className="td">
+          <Link to={`/${EPOCHS}/${item.number}/${TXNS}`}>{item.stats.current.transactions}</Link>
+        </div>
+        <div className="td">
+          <Link to={`/${EPOCHS}/${item.number}/${SMESHER}`}>{item.stats.current.smeshers}</Link>
+        </div>
+        <div className="td" style={{ flexGrow: 2 }}>
+          <Link to={`/${EPOCHS}/${item.number}/${REWARDS}`}>
+            {formatSmidge(item.stats.current.rewards)}
+          </Link>
+        </div>
       </div>
-      <div className="td">
-        <Link to={`/${EPOCHS}/${item.number}/${LAYERS}`}>{item.layers}</Link>
-      </div>
-      <div className="td">
-        <Link to={`/${EPOCHS}/${item.number}/${TXNS}`}>{item.stats.current.transactions}</Link>
-      </div>
-      <div className="td">
-        <Link to={`/${EPOCHS}/${item.number}/${SMESHER}`}>{item.stats.current.smeshers}</Link>
-      </div>
-      <div className="td" style={{ flexGrow: 2 }}>
-        <Link to={`/${EPOCHS}/${item.number}/${REWARDS}`}>
-          {formatSmidge(item.stats.current.rewards)}
-        </Link>
-      </div>
-    </div>
-  ))
+    );
+  })
 );
 
 export default observer(EpochsRow);
