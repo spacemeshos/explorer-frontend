d75d9e41b93e85ff970e01a397b9486f6915a9ad
Kacper Sawicki <kacper@ksawicki.pl>
2024-05-09 15:48:55 +0200

    Add new malfeasance type

--- src/components/MalfeasanceBlock/index.jsx
+++ src/components/MalfeasanceBlock/index.jsx
@@ -5,14 +5,14 @@ import { MALFEASANCE } from '../../config/constants';
 import { getColorByPageName } from '../../helper/getColorByPageName';
 
 type Props = {
-    title: string;
-    color: string,
-    desc: string;
-    uiStore: string,
+  title: string;
+  color: string,
+  desc: string;
+  uiStore: string,
 };
 
 const MalfeasanceBlock = (props: Props) => {
-  const { kind, layer } = props;
+  const { kind, layer, debugInfo } = props;
 
   const kindTranslation = {
     MALFEASANCE_ATX: 'ATXs',
@@ -24,17 +24,31 @@ const MalfeasanceBlock = (props: Props) => {
     <div style={{ display: 'grid', paddingBottom: '20px' }}>
       <div className="malfeasanceBlock">
         <div className="malfeasanceBlock-wrap">
-          <p style={{ color: getColorByPageName(MALFEASANCE).textColor }} className="malfeasanceBlock-title">This smesher identity is disqualified from participating in the protocol due to violation of protocol rules.</p>
-          <p className="malfeasanceBlock-desc">
-            The key associated with this smesher was used to sign two contradictory
-            {' '}
-            {kindTranslation[kind]}
-            {' '}
-            in layer
-            {' '}
-            {layer}
-            . This smesher will therefore be excluded from all future protocol decisions and denied any rewards beyond those already received.
+          <p style={{ color: getColorByPageName(MALFEASANCE).textColor }} className="malfeasanceBlock-title">
+            This smesher
+            identity is disqualified from participating in the protocol due to violation of protocol rules.
           </p>
+          {kind === '5' ? (
+            <p className="malfeasanceBlock-desc">
+              {debugInfo.split('cause: ')[1]}
+              <br />
+              This smesher will therefore be excluded from all future protocol decisions and denied any rewards beyond
+              those already received.
+            </p>
+          ) : (
+            <p className="malfeasanceBlock-desc">
+              The key associated with this smesher was used to sign two contradictory
+              {' '}
+              {kindTranslation[kind]}
+              {' '}
+              in layer
+              {' '}
+              {layer}
+              . This smesher will therefore be excluded from all future protocol decisions and denied any rewards beyond
+              those already received.
+            </p>
+          )}
+
           <p className="malfeasanceBlock-link">
             <a href="https://spacemesh.io/blog/community-clarification-equivocation/">READ MORE</a>
           </p>
